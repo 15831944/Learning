e678c46ba6fac516e03f5e7eef9bc7f0bfa2a17c
StarkShang <stark.shang@sjtu.edu.cn>
2016-11-26 23:42:04 +0800

    v2016.11.26

---
+++ CSharp/Json/src/L01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace L01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dir = new DirectoryInfo(".");
+            Console.WriteLine(ConfigInfo.CellRoot);
+        }
+    }
+}
