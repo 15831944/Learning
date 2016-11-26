66581728f3f7af3ba790b9e51b724bf7551331e0
StarkShang <stark.shang@sjtu.edu.cn>
2016-11-27 00:24:07 +0800

    v2016.11.26

---
+++ CSharp/UnitTest/StaticClassTest/src/StaticClass/StaticClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public static class StaticClass
+    {
+        public static string Member = "Hello World";
+    }
+}
