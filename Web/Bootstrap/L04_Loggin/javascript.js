3c26d71184bcb71546964d45373364615edd238d
StarkShang <stark.shang@sjtu.edu.cn>
2016-12-27 21:59:24 +0800

    v2016.12.27

---
+++ Web/Bootstrap/L04_Loggin/javascript.js
@@ -0,0 +1,8 @@
+function login() {
+    document.getElementById('mask').style.display="block";
+    document.getElementById('signupblock').style.display="block";
+}
+function cancel() {
+    document.getElementById('mask').style.display="none";
+    document.getElementById('signupblock').style.display="none";
+}
